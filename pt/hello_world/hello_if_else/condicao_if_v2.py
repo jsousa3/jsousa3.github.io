c5849c8c37a4ae1f2cd3e44ce5624948172fb318
jsousa3 <jsousa@deetc.isel.ipl.pt>
2022-12-14 11:12:30 +0000

    removed everything

--- pt/hello_world/hello_if_else/condicao_if_v2.py
+++
@@ -1,8 +0,0 @@
-# assume-se que a quantidade é 1
-desconto = desconto_1
-mensagem = 'na compra de 1 chocolate o valor a pagar é:'
-
-if quantidade == 2: # se a quantidade for 2
-    # altera-se o desconto e a mensagem
-    desconto = desconto_2
-    mensagem = 'na compra de 2 chocolates o valor a pagar por cada um é:'
--- pt/hello_world/hello_variaveis/exemplo_variaveis.py
+++
@@ -1,4 +0,0 @@
-preco_1     = 5.2
-preco_2     = 7.3
-preco_total = preco_1 + preco_2
-print(preco_total)
