3e3b6f359276cd8c315e1d6458e54db45b617d85
jbs <joao.beleza.sousa@gmail.com>
2024-11-22 11:06:16 +0000

    fstrings output files

--- pt/strings/fstrings_nomes_variaveis.py
+++ pt/strings/fstrings_nomes_variaveis.py
@@ -2,10 +2,15 @@ moeda                   = 'EUR'
 preço                   = 20
 desconto_em_percentagem = 0.1
 preço_a_pagar           = 18
-s1 = f'''
-{moeda = }
-{preço = }
-{desconto_em_percentagem = }
-{preço_a_pagar = }
-'''
-print(s1)
+
+# sem f-strings
+print('moeda =', moeda)
+print('preço =', preço)
+print('desconto_em_percentagem =', desconto_em_percentagem)
+print('preço_a_pagar =', preço_a_pagar)
+
+# com f-strings (esta versão dá muito menos trabalho a escrever)
+print(f'{moeda = }')
+print(f'{preço = }')
+print(f'{desconto_em_percentagem = }')
+print(f'{preço_a_pagar = }')
